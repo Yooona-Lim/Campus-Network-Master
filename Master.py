91030b8834dbf64f4cc4faa40bc2b0786be50d64
Yooona-Lim <81098819+Yooona-Lim@users.noreply.github.com>
2023-06-10 09:55:52 +0800

    Edited the output style

--- Master.py
+++ Master.py
@@ -40,29 +40,30 @@ class network_master:
             }
 
         response = requests.get(url=url_login, params=data_login, headers=headers)
-        print(f'request status code:\033[0;30;42m{response.status_code}\033[0m')
+        print(f'request url: \033[0;37;44m{url_login}\033[0m')
+        print(f'request status code: \033[0;37;42m {response.status_code} \033[0m')
         # print('login request text:', response.text)
         # 提取括号内的 JSON 数据
         json_data = response.text[response.text.index('(') + 1:response.text.rindex(')')]
         # 解析 JSON 数据
         data = json.loads(json_data)
-        print(f'json.msg: \033[0;30;41m{data["msg"]}\033[0m')
+        print(f'json.msg: \033[0;37;41m{data["msg"]}\033[0m')
 
 if __name__ == "__main__":
     try:
         print('Reading the executable filename...')
         system = platform.system()
         # \033[字体显示方式;字体颜色;字体背景色m'字符'\033[0m
-        print(f"Running on \033[5;30;43m{system}\033[0m OS");
+        print(f"Running on \033[5;30;43m {system} \033[0m OS");
         filename = os.path.basename(sys.argv[0])
         if system == 'Windows':
             filename = os.path.splitext(filename)[0]
-        print(f'Executable filename:\033[0;30;47m{filename}\033[0m')
+        print(f'Executable filename: \033[0;30;47m{filename}\033[0m')
 
         # Extracting id and passwd from the filename
         id, passwd = filename.split(';')
-        print(f'id:\033[0;30;45m{id}\033[0m')
-        print(f'passwd:\033[0;30;46m{passwd}\033[0m')
+        print(f'id: \033[0;37;45m{id}\033[0m')
+        print(f'passwd: \033[0;30;46m{passwd}\033[0m')
 
         master = network_master(id, passwd)
 
